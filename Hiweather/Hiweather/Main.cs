64b10377e0c62e533ff13d5baf418c827d04971d
Han <lhu3319@nate.com>
2018-11-14 12:12:00 +0900

    버튼,라벨 클래스 생성

--- Hiweather/Hiweather/Main.cs
+++ Hiweather/Hiweather/Main.cs
@@ -19,21 +19,16 @@ namespace Hiweather
             Load += Main_Load;
         }
         FlowLayoutPanel flowLayoutPanel1 = new FlowLayoutPanel();
-        Button button = new Button();
+        Button button;
         
          private void Main_Load(object sender, EventArgs e)
         {
             
            DrawFLP();
-           DrawBTN("feedfack","피드백");
-            DrawBTN("option","설정");
-             
-             
-
-            flowLayoutPanel1.Controls.Add(button);
-            
-
-
+           DrawBTN("feedback","피드백",600,0);
+           
+           flowLayoutPanel1.Controls.Add(button);
+          
            Controls.Add(flowLayoutPanel1);
             
         }
@@ -51,12 +46,15 @@ namespace Hiweather
            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
         }
-        private void DrawBTN(string b_name,string b_text)
+        private void DrawBTN(string b_name,string b_text,int x, int y)
         {
-            button.Location = new System.Drawing.Point(600, 0);
+            button = new Button();
+            button.DialogResult = DialogResult.OK;
+            button.Location = new System.Drawing.Point(x, y);
             button.Name = b_name;
             button.Size = new System.Drawing.Size(75, 23);
            button.Text = b_text;
+            button.Cursor = Cursors.Hand;
             button.UseVisualStyleBackColor = true;
         }
        
